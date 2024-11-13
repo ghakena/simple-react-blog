35e154339cd1b1a41a9371b76ea0cd05f768102e
Gibbs Herbert Akena <akenagibbsherbert6@gmail.com>
2024-11-13 17:36:17 +0300

    added states to handle search and posts to App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,38 @@ import PostPage from './PostPage';
 import About from './About';
 import Missing from './Missing';
 import { Routes, Route, useHistory } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My 1st post",
+      datetime: "November 13, 2024 5:33:36 AM",
+      body: "lorem ipsum dolor..."
+    },
+    {
+      id: 2,
+      title: "My 2nd post",
+      datetime: "November 13, 2024 6:33:36 AM",
+      body: "lorem ipsum dolor..."
+    },
+    {
+      id: 3,
+      title: "My 3rd post",
+      datetime: "November 13, 2024 12:33:36 PM",
+      body: "lorem ipsum dolor..."
+    },
+    {
+      id: 4,
+      title: "My 4th post",
+      datetime: "November 13, 2024 5:35:31 PM",
+      body: "lorem ipsum dolor..."
+    }
+  ]);
+  
+  const [search, setSearch] = useState('');
+
   return (
     <div className="App">
       <Header title="React.js Blog"/>
