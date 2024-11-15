1cd4383cd6051955191acd790db89a7296c69605
Gibbs Herbert Akena <akenagibbsherbert6@gmail.com>
2024-11-15 15:50:26 +0300

    completed definition of Missing component

--- src/Missing.js
+++ src/Missing.js
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const Missing = () => {
     return (
-      <main>
-          Missing
+      <main className="Missing">
+          <h2>Page Not Found</h2>
+          <p>Oops! Better luck next time.</p>
+          <p>
+            <Link to='/'>Get back home here!</Link>
+          </p>
       </main>
     )
 }
-  
+
 export default Missing;
