8ca39fc81dc235954f184897e86c9137b6eb6e6f
Gibbs Herbert Akena <akenagibbsherbert6@gmail.com>
2024-11-15 15:58:06 +0300

    completed footer component

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,11 @@
 const Footer = () => {
-    return (
-      <footer>
-          Footer
-      </footer>
-    )
+  const today = new Date();
+
+  return (
+    <footer className="Footer">
+        <p>Copyright &copy; {today.getFullYear()}</p>
+    </footer>
+  )
 }
   
 export default Footer;
