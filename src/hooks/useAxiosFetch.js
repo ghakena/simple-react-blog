8b2f788a3a1877fe883c636f17be71338a9421dd
Gibbs Herbert Akena <akenagibbsherbert6@gmail.com>
2024-11-21 13:26:37 +0300

    fixing merge conflict

--- src/hooks/useAxiosFetch.js
+++ src/hooks/useAxiosFetch.js
@@ -1,16 +1,1 @@
-import { useState, useEffect } from 'react';
-<<<<<<< HEAD
-
-=======
-import axios from 'axios';
-
-const useAxiosFetch = (dataUrl) => {
-    const [data, setData] = useState([]);
-    const [fetchError, setFetchError] = useState(null);
-    const [isLoading, setIsLoading] = useState(null);
-
-    useEffect(() => {
-        
-    }, [])
-}
->>>>>>> custom-hooks
+import { useState, useEffect } from 'react'
