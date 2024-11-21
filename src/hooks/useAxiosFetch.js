af7b6190d22c39f2f4319c3fb56a73c178dc7043
Gibbs Herbert Akena <akenagibbsherbert6@gmail.com>
2024-11-21 13:22:22 +0300

    defined key imports and initialized states for data, error and loading functionalities

---
+++ src/hooks/useAxiosFetch.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useAxiosFetch = (dataUrl) => {
+    const [data, setData] = useState([]);
+    const [fetchError, setFetchError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+
+    useEffect(() => {
+        
+    }, [])
+}
