0c514b911828c6691bc2ad7cc9b2cd41e43ce09e
Gibbs Herbert Akena <akenagibbsherbert6@gmail.com>
2025-01-13 20:38:45 +0300

    added more comments to the useAxiosFetch hook

--- src/hooks/useAxiosFetch.js
+++ src/hooks/useAxiosFetch.js
@@ -8,18 +8,23 @@ const useAxiosFetch = (dataUrl) => {
 
     useEffect(() => {
         // deal with memory leak.
+        // we want the component to be mounted, and not attempt to apply something when it is unmounted, which would result in a memory leak. we will keep checking this status as we apply things.
         let isMounted = true;
         // to cancel a request for whatever reason, a component is unmounted.
+        // this is in essence a cancellation token for axios.
         const source = axios.CancelToken.source();
 
+        // define function to fetch data.
         const fetchData = async (url) => {
             setIsLoading(true);
             try {
                 const response = await axios.get(url, {
                     // apply cancel token. cancel request when we unmount our component.
+                    // this cancel token is sent with the request.
                     cancelToken: source.token
-                })
+                });
 
+                // once we get a response after sending the request.
                 // if component is mounted
                 if (isMounted) {
                     setData(response.data);
@@ -41,6 +46,7 @@ const useAxiosFetch = (dataUrl) => {
 
         // data clean-up function.
         // will cancel the request if the component is unloaded.
+        // the clean-up function will only run when a dependency for the custom hook changes.
         const cleanUp = () => {
             console.log('cleaning up...');
             isMounted = false;
