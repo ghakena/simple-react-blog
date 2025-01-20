e2cdd1b005676c22442e7c9281339525ba9bc6aa
Gibbs Herbert Akena <akenagibbsherbert6@gmail.com>
2025-01-20 09:15:30 +0300

    edited formatting

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 const useWindowSize = () => {
     // state to handle the window size i.e. width and height.
