5d5e619092d27675c27a930b956a7358e8672f00
Gibbs Herbert Akena <akenagibbsherbert6@gmail.com>
2025-01-20 11:11:51 +0300

    commenced refactor to incorporate the use of the context api inside app.js

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,9 +1,10 @@
 import { createContext, useEffect, useState } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { format } from 'date-fns';
-import api from './api/posts';
-import useWindowSize from './hooks/useWindowSize';
-import useAxiosFetch from './hooks/useAxiosFetch';
+// 2 dots as you're getting out of the context folder first before accessing api and hooks folder.
+import api from '../api/posts';
+import useWindowSize from '../hooks/useWindowSize';
+import useAxiosFetch from '../hooks/useAxiosFetch';
 
 const DataContext = createContext({});
 
