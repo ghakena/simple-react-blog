54f9df462ddd49bb532dc85633155c66b72498d2
Gibbs Herbert Akena <akenagibbsherbert6@gmail.com>
2025-01-20 12:33:49 +0300

    edited comment in Header.js

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import { FaLaptop, FaTabletAlt, FaMobileAlt } from 'react-icons/fa';
 import useWindowSize from './hooks/useWindowSize';
 
 const Header = ({ title}) => {
-    // pass to useContext the context you will be using.
+    // destructure width from useWindowSize custom hook.
     const { width } = useWindowSize();
 
     return (
