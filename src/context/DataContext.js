3f09feb3a8e15c35c67f4e051beb26ca5fa5590d
Gibbs Herbert Akena <akenagibbsherbert6@gmail.com>
2025-01-20 11:14:12 +0300

    moved states and the useEffect hooks to DataContext.js

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -9,6 +9,49 @@ import useAxiosFetch from '../hooks/useAxiosFetch';
 const DataContext = createContext({});
 
 export const DataProvider = ({ children }) => {
+    const [posts, setPosts] = useState([]);
+    const [search, setSearch] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [postTitle, setPostTitle] = useState('');
+    const [postBody, setPostBody] = useState('');
+    const [editPostTitle, setEditPostTitle] = useState('');
+    const [editPostBody, setEditPostBody] = useState('');
+    // destructure and pull out the "width" from the imported useWindowSize custom hook below.
+    const { width } = useWindowSize();
+    // destructure useAxiosFetch to extract important items.
+    const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
+
+    const navigate = useNavigate(); // this replaces useHistory from v5.
+
+    // useEffect to fetch our data. will run at load time, so it will have an empty dependency array
+    // useEffect(() => {
+    //   // function to fetch data using the axios api.
+    //   const fetchData = async () => {
+    //     try {
+    //       const response = await api.get('/posts');
+    //       if (response && response.data) {
+    //         setPosts(response.data)
+    //       }
+    //     } catch (err) {
+    //       if (err.response) {
+    //         // response not in the 200 response range
+    //         console.log(err.response.data);
+    //         console.log(err.response.status);
+    //         console.log(err.response.headers);
+    //       } else {
+    //         // if we did not receive err.response
+    //         console.log(`Error: ${err.message}`);
+    //       }
+    //     }
+    //   }
+        
+    //   fetchData();
+    // }, [])
+
+    useEffect(() => {
+        // set the posts as the data being received from the local json-server endpoint using the useAxiosFetch custom hook. The dependency array will hold the data. The useEffect will run whenever the data changes.
+        setPosts(data);
+    }, [data])
     return (
         <DataContext.Provider value={{
 
